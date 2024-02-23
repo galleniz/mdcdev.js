fd1757d0b00f263dd3eac38512b825eb94620ede
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-02-23 17:48:32 -0600

    trying smthin | update  test.ts

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ import Client from "./src/Client"
 console.log("when you see this, you know you are in the right place!")
 
 const client = new Client();
-client.hi()
 
-
-
+client.discord.users.avatar("482637805034668032").then((res) => {
+    console.log(res);
+});
