055b188683f224a0840d19b3fc080e641ce1d79b
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-02-23 17:48:32 -0600

    trying smthin | update  scripts/PublishGit.ts

--- scripts/PublishGit.ts
+++ scripts/PublishGit.ts
@@ -13,12 +13,35 @@ async function main() {
 }
 async function publishToGit() {
     Log.log("Trying to add all files...")
-    execSync('git add .');
-    Log.log("Added all files!\nTrying to commit...")
-    execSync(`git commit -m "${commitMessage.replace("\"", "\\\"")}"`);
-    Log.log("Committed!\nTrying to push...")
-    execSync('git push');
-    Log.log("Pushed!")
+    // Get files, and add them each updating with commitMessage + " update " + fileName, then push
+    // example: if an file was modified, it will be added to the commit, and the commit message will be "auto commit update example.ts"
+
+    // get files
+    const files = execSync("git status -s").toString().split("\n").filter(f => f.trim() !== "" );
+
+    Log.log("Files: " + files.join(", "));
+
+    // Add each file
+    files.forEach(file => {
+        var fileStatus = file.slice(0, 1);
+        var filePath = file.slice(2);
+        Log.info("Adding file: " + filePath);
+        execSync(`git add ${filePath}`);
+        // comit it
+        Log.info("Committing file: " + file);
+        // if file is created put "create" instead of "update", or if it was deleted put "delete"
+
+        console.log(filePath);
+        var type = fileStatus.startsWith("A") ? "create" : fileStatus.startsWith("D") ? "delete" : "update";
+      
+        execSync(`git commit -m "${commitMessage} | ${type} ${filePath}"`);
+    });
+
+    // push
+    execSync("git push");
+
+    
+
 }
 
 
