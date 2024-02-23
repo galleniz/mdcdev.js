7dd3a4c5d0c0444bb9c345705e50572dcf0a9873
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-02-23 17:48:32 -0600

    trying smthin | update  src/v2/twitter/users.ts

--- src/v2/twitter/users.ts
+++ src/v2/twitter/users.ts
@@ -2,26 +2,49 @@ import BaseRoot from "../../BaseRoot";
 import Client from "../../Client";
 import { LoggerXD } from "../../utils/Logger";
 
+/**
+ * Represents a class for interacting with Twitter users.
+ * @class
+ * @extends BaseRoot
+ */
 export default class TwitterUsers extends BaseRoot {
+    /**
+     * Creates an instance of TwitterUsers.
+     * @constructor
+     * @param {Client} client - The client object used for making API requests.
+     */
     constructor(client: Client) {
-        // private access to api keys in client
         super(client.getApiKeys(), client);
         if (this.client.debug)
         LoggerXD.info("TwitterUsers class has been initialized");
     }
 
-    public async get(username: string = "@me") {
-        // yo toda una ingeniera en software:
+    /**
+     * Retrieves information about a Twitter user.
+     * @param {string} [username="@me"] - The username of the Twitter user. Defaults to "@me" which represents the authenticated user.
+     * @returns {Promise<any>} - A promise that resolves with the user information.
+     */
+    public async get(username: string = "@me"): Promise<any> {
         return await this.client.request(`https://api.mdcdev.me/v2/twitter/users/${username}`, "GET");
     }
-    public async avatar(username: string = "@me") {
+
+    /**
+     * Retrieves the avatar of a Twitter user.
+     * @param {string} [username="@me"] - The username of the Twitter user. Defaults to "@me" which represents the authenticated user.
+     * @returns {Promise<any>} - A promise that resolves with the avatar image data.
+     */
+    public async avatar(username: string = "@me"): Promise<any> {
         const res = await this.client.request(`https://api.mdcdev.me/v2/twitter/users/${username}/avatar`, "GET");
-        delete res?.data
         return res;
     }
-    public async banner(username: string = "@me")  {
+
+    /**
+     * Retrieves the banner image of a Twitter user.
+     * @param {string} [username="@me"] - The username of the Twitter user. Defaults to "@me" which represents the authenticated user.
+     * @returns {Promise<any>} - A promise that resolves with the banner image data.
+     */
+    public async banner(username: string = "@me"): Promise<any> {
         const res = await this.client.request(`https://api.mdcdev.me/v2/twitter/users/${username}/banner`, "GET");
-        delete res?.data
         return res;
     }
 }
