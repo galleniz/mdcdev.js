f57436a67389ac7ec69fbb172c59265a6dedaf3a
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-02-23 13:08:10 -0600

    Adding docs to da interfaces

--- src/utils/Interafces.ts
+++ src/utils/Interafces.ts
@@ -1,3 +1,6 @@
+/**
+ * Represents the API keys for various services.
+ */
 export interface ApiKeys {
     twitter_api_key: string;
     discord_api_key: string;
@@ -5,26 +8,49 @@ export interface ApiKeys {
     github_api_key: string;
 
 }
+/**
+ * Represents the configuration options.
+ */
 export interface GayFont {
     font: string;
     color: string;
     size: number;
 
 }
+/**
+ * Represents the configuration options, but misc !!!
+ */
 export interface MiscConfig {
     debug: boolean;
     reallyDisableCache: boolean;
 }
+/**
+ * Represents the configuration options, but, iknow iknow, cache !!!
+ */
 export interface CacheConfig {
     allowCache: boolean;
     cacheTime: number;
 }
+/**
+ * Why this is called EventEmitterResponse? I don't know, but it's the response of the EventEmitter.
+ */
 export interface EventEmitterResponse { 
     url: string;
     data: any;
     usedCache: boolean;
 }
+/**
+ * Uy UY UY, this is the config of the client
+ */
 export interface Configs {
     misc: MiscConfig;
     cache: CacheConfig;
 }
+/**
+ * Im funi, right? :D
+ * 
+ * Okay, this isnt necessary, but, i like it uwu
+ * can you ignore this?
+ * please...
+ */
+export const Si = "no" 
