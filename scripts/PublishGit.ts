608c2bbc9cfcf9d151f65f21063dda092fea144c
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-02-23 12:59:49 -0600

    Addingpublishcommentcommit,iswear

--- scripts/PublishGit.ts
+++ scripts/PublishGit.ts
@@ -2,8 +2,10 @@
 
 import { execSync } from 'child_process';
 import Log from './src/Log';
-
-
+// obtain the args!!
+const args = process.argv.slice(2);
+const commitMessage = args.join("") || "auto commit";
+Log.info("Commit message: " + commitMessage);
 async function main() {
     Log.log("Publishing to git...")
     await publishToGit();
@@ -13,7 +15,7 @@ async function publishToGit() {
     Log.log("Trying to add all files...")
     execSync('git add .');
     Log.log("Added all files!\nTrying to commit...")
-    execSync('git commit -m "auto commit"');
+    execSync(`git commit -m "${commitMessage.replace("\"", "\\\"")}"`);
     Log.log("Committed!\nTrying to push...")
     execSync('git push');
     Log.log("Pushed!")
