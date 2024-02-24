7bda09e4d7ed118e6d9cac2e35110a07c87c5b00
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-02-24 13:36:04 -0600

    uwu | update test.ts

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
 import Client from "./src/Client"
 
-console.log("when you see this, you know you are in the right place!")
-
 const client = new Client();
 
 client.discord.users.avatar("482637805034668032").then((res) => {
     console.log(res);
 });
+
+console.log("test");
