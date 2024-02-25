78a36f8c94ecd013cbe03ee10320e53764366d8b
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-02-24 19:51:44 -0600

    Documentation Update  | Update src/v2/discord/invites.ts

--- src/v2/discord/invites.ts
+++ src/v2/discord/invites.ts
@@ -1,5 +1,6 @@
 import BaseRoot from "../../BaseRoot";
 import Client from "../../Client";
+import { Invite } from "../../utils/Interfaces";
 import { LoggerXD } from "../../utils/Logger";
 
 export default class DiscordInvites extends BaseRoot {
@@ -12,7 +13,7 @@ export default class DiscordInvites extends BaseRoot {
 
     public async get(code: string = "dae") {
         // yo toda una ingeniera en software:
-        return await this.client.request(`https://api.mdcdev.me/v2/discord/invites/${code}`, "GET");
+        return await this.client.request<Invite, string>(`https://api.mdcdev.me/v2/discord/invites/${code}`, "GET");
     }
 
 }
