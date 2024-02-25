3eb70f5e7942ff2463f0b923f55bbc0969e3a047
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-02-24 19:51:44 -0600

    Documentation Update  | Update test.ts

--- test.ts
+++ test.ts
@@ -2,8 +2,6 @@ import Client from "./src/Client"
 
 const client = new Client();
 
-client.discord.users.avatar("482637805034668032").then((res) => {
-    console.log(res);
-});
+client.twitter.users.get("mdc_dev").then(console.log);
 
 console.log("test");
